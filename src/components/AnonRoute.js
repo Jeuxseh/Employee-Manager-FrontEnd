1654873e31affcf94555e4fa6466f210fad6eb1b
Marc Benaiges <benaiges.marc@gmail.com>
2019-03-28 17:09:19 +0100

    bug fixed

--- src/components/AnonRoute.js
+++ src/components/AnonRoute.js
@@ -10,7 +10,7 @@ const AnonRoute = ({ component: Component, isLogged, ...rest }) => {
         if (!isLogged) {
           return <Component {...props} />
         } else {
-          return <Redirect to={{ pathname: '/private', state: { from: props.location } }} />
+          return <Redirect to={{ pathname: '/', state: { from: props.location } }} />
         }
       }
       }
