2dc124bf426e12de0c2704a144ad9d5d2003ea5f
Marc Benaiges <benaiges.marc@gmail.com>
2019-03-30 20:27:47 +0100

    particular schedule

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -5,8 +5,7 @@ class Calendar extends Component {
     return (
       <div>
         <h1>calendar</h1>
-        <h3>calendario de todos los workers
-        </h3>
+        <h3>calendario de todos los workers</h3>
       </div>
     );
   }
--- src/pages/EmployeeCalendar.js
+++ src/pages/EmployeeCalendar.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import employeeService from '../services/employeeServices';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 class EmployeeCalendar extends Component {
 
   state = {
-    data: {}
-    
+    data: {},
+    currentInitHour: '',
+    currentEndHour: '',
+    counterDay: 0,
+    currentDay: '',
+    formatDate: '',
+    isLoading: true
   }
 
   componentDidMount() {
-    this.getOneEmployee();
+    this.getOneEmployee()
   }
 
   getOneEmployee = () => {
@@ -19,50 +25,107 @@ class EmployeeCalendar extends Component {
     employeeService.getEmployee(id)
       .then(data => {
         this.setState({
-          data
+          data,
+          isLoading: false
         })
+        this.getCurrentDay();
+        this.getSchedule(this.state.data.schedule);
+        this.getFormatDay();
       })
       .catch(error => console.log(error))
   }
 
-  getSchedule = (obj) => {
-    
+  getCurrentDay = () => {
     var currentDate = new Date();
-    currentDate.setDate(currentDate.getDate());
+    currentDate.setDate(currentDate.getDate() + this.state.counterDay);  // <- Para modificar la fecha
+    this.setState({
+      currentDay: currentDate
+    })
+  }
 
-    var day = currentDate.toString().substr(0,3).toLocaleLowerCase();
-    for (var key in obj) { 
-      if (key.toString().substr(0,3) === day){
-        console.log(obj[key])
-        const initHour = obj[key].initHour;
-        const endHour = obj[key].endHour;
-        console.log(initHour)
-        console.log(endHour)
-      } 
+  getSchedule = (obj) => {
+    var day = this.state.currentDay.toString().substr(0, 3).toLocaleLowerCase();
+    for (var key in obj) {
+      if (key.toString().substr(0, 3) === day) {
+        // console.log(obj[key])
+        this.setState({
+          currentInitHour: obj[key].initHour,
+          currentEndHour: obj[key].endHour
+        })
+      }
     }
+  }
+
+  getFormatDay = () => {
+    var date = this.state.currentDay.toISOString().substr(0, 10);
+    this.setState({
+      formatDate: date
+    })
+  }
+
+  countDown = async () => {
+    await this.setState({
+      counterDay: this.state.counterDay - 1,
+    })
+    try {
+      this.getCurrentDay()
+      this.getFormatDay()
+      this.getSchedule(this.state.data.schedule)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  countUp = async () => {
+    await this.setState({
+      counterDay: this.state.counterDay + 1
+    });
+    try {
+      this.getCurrentDay()
+      this.getFormatDay()
+      this.getSchedule(this.state.data.schedule)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  dateChange = (event) => {
+
+    const date = new Date();
+    const day = date.getDate();
+    console.log(date)
   }
 
 
   render() {
-    const { _id, username, schedule } = this.state.data;
+    const { _id, username } = this.state.data;
+    const { isLoading } = this.state;
 
     var currentDate = new Date();
-    currentDate.setDate(currentDate.getDate());
-
-    var day = currentDate.toString().substr(0,3).toLocaleLowerCase();
+    currentDate.setDate(currentDate.getDate() + this.state.counterDay);
     var date = currentDate.toISOString().substr(0, 10);
 
-  
-    return (
-      <div>
-        {this.getSchedule(schedule)}
-        <Link to={`/employee/${_id}`} >Profile</Link>
-        <h1>Calendar {username}</h1>
-        <input onchange={this.dateChange}id="dateRequired" type="date" name="dateRequired" defaultValue={date} />
+    switch (isLoading) {
+      case true:
+        return "Loading....";
+      case false:
+        return (
+          <div>
+            <Link to={`/employee/${_id}`} >Profile</Link>
+            <h1>Calendar {username}</h1>
+            <button onClick={this.countDown}><FontAwesomeIcon icon="caret-square-left" /></button>
+            {/* <input onChange={this.dateChange} id="dateRequired" type="date" name="dateRequired" value={this.state.formatDate}/> */}
+            <p>{this.state.formatDate}</p>
 
-        <h2>Schedule</h2>        
-      </div>
-    );
+            <button onClick={this.countUp}><FontAwesomeIcon icon="caret-square-right" /></button>
+            <h2>Schedule</h2>
+            <p>Hora Inicio: {this.state.currentInitHour}</p>
+            <p>Hora Fin: {this.state.currentEndHour}</p>
+          </div>
+        );
+      default:
+        break;
+    }
   }
 }
 
