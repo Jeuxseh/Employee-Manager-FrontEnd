92549f3b1d92df55131d3911acb882120fe9142f
Jesús <jeusxe@hotmail.com>
2019-04-01 16:11:21 +0200

    sort of employee status done

--- src/components/EmployeesList.js
+++ src/components/EmployeesList.js
@@ -7,26 +7,90 @@ import '../stylesheets/employee-card-list.css'
 class EmployeesList extends Component {
 
   state = {
-    data: []
+    data: [],
+    employees: [],
   }
   // Todo añadir estado del loading para indicar que esta carganado
   componentDidMount() {
     this.getAllEmployees();
+    this.sortEmployees(this.state.employees)
   }
+  // componentDidUpdate(){
+  //   this.getAllEmployees();
+  // }
 
   getAllEmployees = () => {
     employeeServices.getAll()
       .then(data => {
-        this.setState({
-          data
+       
+
+        data.forEach((employee,index) => {
+          this.compareDay(employee,index)
         })
+
+        const newData = this.sortEmployees(data);
+
+        this.setState({
+          data:newData, 
+        })
+
+
       })
       .catch(err => console.log(err));
   }
 
+  sortEmployees = (employees) => {
+    const newData = employees.sort((x,y) => {
+      if(x.stillWorking === true && y.stillWorking !== true){
+        return -1;
+      }
+
+      if(x.stillWorking !== true && y.stillWorking === true){
+        return 1;
+      }
+      
+      if(x.stillWorking === true && y.stillWorking === true){
+        return 0;
+      }
+
+      if(x.isWorkingToday === true && y.isWorkingToday !== true){
+        return -1;
+      }
+
+      if(x.isWorkingToday !== true && y.isWorkingToday === true){
+        return 1;
+      }
+
+      if(x.isWorkingToday !== true && y.isWorkingToday !== true){
+        return 0;
+      }
+
+      return 0;
+    })
+    return newData;
+  }
+
+  compareDay = (employee,index) => {
+    var currentDate = new Date();
+    var day = currentDate.toString().substr(0, 3).toLocaleLowerCase();
+    const currentTime = currentDate.toString().substr(15, 16).split(':').map(timer => parseInt(timer)).splice(0, 2)
+    for (var key in employee.schedule) {
+      const finishHour = employee.schedule[key].endHour.split(':').map(timer => parseInt(timer));
+      const initialHour = employee.schedule[key].initHour.split(':').map(timer => parseInt(timer));
+      if (key.toString().substr(0, 3) === day && employee.schedule[key].initHour !== "") {
+        employee.isWorkingToday = true;
+        if (((currentTime[0] === initialHour[0] && currentTime[1] >= initialHour[1])
+          || (currentTime[0] > initialHour[0]))
+          && ((currentTime[0] === finishHour[0] && currentTime[1] <= finishHour[1])
+            || (currentTime[0] < finishHour[0]))) {
+          employee.stillWorking = true
+        }
+      }
+    }
+  }
+
   render() {
     return (
-      <>
         <ul>
           {this.state.data.map(employee => (
             <EmployeeCard 
@@ -35,7 +99,6 @@ class EmployeesList extends Component {
             />
           ))}
         </ul>
-      </>
     );
   }
 }
